c8d1a0c78d7409bb04d145609e064cc319a48619
Xibalba <xibalbazedd@gmail.com>
2016-05-25 19:41:55 +0200

    code cleanup

--- src/eu/reborn_minecraft/zhorse/commands/Command.java
+++ src/eu/reborn_minecraft/zhorse/commands/Command.java
@@ -226,11 +226,9 @@ public class Command {
 	
 	protected String getRemainingClaimsMessage(UUID playerUUID) {
 		String message = "";
-		//if (samePlayer || isPlayerOnline(playerUUID, true)) {
-			int claimsAmount = zh.getUM().getClaimsAmount(playerUUID);
-			int maxClaims = zh.getCM().getClaimsLimit(playerUUID);
-			message = zh.getMM().getMessageAmountMax(s, LocaleEnum.remainingClaimsFormat, claimsAmount, maxClaims, true);
-		//}
+		int claimsAmount = zh.getUM().getClaimsAmount(playerUUID);
+		int maxClaims = zh.getCM().getClaimsLimit(playerUUID);
+		message = zh.getMM().getMessageAmountMax(s, LocaleEnum.remainingClaimsFormat, claimsAmount, maxClaims, true);
 		return message;
 	}
 	
